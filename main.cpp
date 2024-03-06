6bee33f04209161f5971c8c0d8dbeb6470fea191
Crocussys <egor.vladislavovich.nn@gmail.com>
2024-03-06 10:27:11 +0300

    done

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <thread>
+
+using namespace std;
+
+int main()
+{
+    const int n = 12;
+    int* arr = new int[n];
+    double ans = 0;
+    const int processor_count = thread::hardware_concurrency();
+    for (int i = 0; i < n; i++){
+        // arr[i] = rand() % 10;
+        arr[i] = i;
+    }
+#pragma omp parallel num_threads(processor_count) reduction(+:ans)
+    {
+#pragma omp for
+        for (int i = 0; i < n; i++){
+            ans += arr[i];
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
